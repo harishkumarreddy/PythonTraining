c79652adef431ab8abf82b778a3f673fa07004ca
Harishkumar Reddy <harishkumarreddy.cherla@gmail.com>
2021-07-23 09:18:54 +0530

    Day 4 class files

---
+++ DataTypes/Qna.py
@@ -0,0 +1,10 @@
+message = "Hello World!#"
+print(message[2:7])
+print(message[-5:-1])
+print(message[-5:-0])
+print(message[::-1])
+
+print(message.count("ll"))
+
+message_2 = 'hello\n\tworld'
+print(message_2.replace("\n", ""))
---
+++ DataTypes/numbers.py
@@ -0,0 +1,48 @@
+# 25
+# 2.55555
+import random
+
+name = "Harishkumar"
+age = 33
+height = 5.9
+
+print(age)
+print(height)
+
+print(f"Hello there, my name is {name} and i am {age} years old.")
+
+n = -3.141652
+rounded_n = round(n, 2)
+rounded_n2 = round(n, 4)
+print(rounded_n)
+print(rounded_n2)
+print(round(age, 2))
+neg_n = -10
+print(neg_n)
+print(abs(neg_n))
+print(abs(n))
+
+print(random.random()*100)
+print(random.randrange(1, 10))
+print(range(10))
+print(range(5, 10))
+print(range(5, 10, 2))
+print(type(range(5, 10, 2)))
+sir = [n for n in range(5, 10, 2)]
+print(sir)
+
+print(type(sir))
+
+is_positive = True
+can_enter = False
+is_open= True
+print(is_positive)
+print(type(is_positive))
+
+age_as_str = str(age)
+print(age_as_str)
+print(type(age_as_str))
+
+ne_num = "25"
+print(ne_num)
+print(type(int(ne_num)))
